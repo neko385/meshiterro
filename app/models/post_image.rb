325fe24b43cb30f4de1e78e3c171ee7249b6b2ab
neko385 <102905708+neko385@users.noreply.github.com>
2022-05-08 14:25:50 +0000

    [Add]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,7 +4,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :shop_name, presence: true
   validates :image, presence:true
 
